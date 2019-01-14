65fc0ae09ac756424130d0a97a20aa39ad71088b
Mattias Norell <mattias.norell@gmail.com>
2019-01-14 22:40:22 +0100

    Added some files

---
+++ Epg/EpgChannel.cs
@@ -0,0 +1,15 @@
+using System.Xml.Serialization;
+
+namespace CouchpotatoShared.Epg
+{
+    public class EpgChannel{
+        [XmlAttribute(AttributeName="id")]
+        public string Id{get;set;}
+        
+        [XmlElement(ElementName="display-name")]
+        public string DisplayName {get;set;}
+
+        [XmlElement(ElementName="url")]
+        public string Url{get;set;}
+    }
+}
